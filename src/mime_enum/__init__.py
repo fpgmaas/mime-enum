139730c95d95bc78195481f0897186d9dfbf1a6e
Florian Maas <fpgmaas@gmail.com>
2025-09-04 22:17:33 +0200

    init commit

---
+++ src/mime_enum/__init__.py
@@ -0,0 +1,6 @@
+from mime_enum.core import parse, try_parse, from_extension, from_path
+from mime_enum.mimetype import MimeType
+
+__all__ = (
+    "MimeType", "parse", "try_parse", "from_extension", "from_path"
+)
